18e65ab6ba86eaf911a41816aa008f54b83f0545
adnaneaabbar <adnaneaabbar@gmail.com>
2021-12-02 22:28:56 +0100

    94% sgd 98% adam 98% rmsprop

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from keras.datasets import mnist
 from keras.utils import to_categorical
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
+import matplotlib.pyplot as plt
 
 batch_size = 128
 num_classes = 10
 epochs = 20
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-val_data = (x_test, y_test)
 
 x_train = x_train.reshape(60000, 784)
 x_test = x_test.reshape(10000, 784)
@@ -21,7 +21,7 @@ x_test /= 255
 y_train = to_categorical(y_train, num_classes)
 y_test = to_categorical(y_test, num_classes)
 
-# try relu, leakyrelu
+# try relu, leaky relu
 model = Sequential()
 model.add(Dense(512, activation='relu', input_shape=(784,)))
 model.add(Dropout(0.2))
@@ -33,14 +33,14 @@ model.summary()
 
 # try sgd, RMSProp, adam
 model.compile(loss='categorical_crossentropy',
-              optimizer='sgd',
+              optimizer='rmsprop',
               metrics=['accuracy'])
 
 model.fit(x_train,
           y_train,
           batch_size=batch_size,
           epochs=epochs,
-          validation_data=val_data)
+          validation_data=(x_test, y_test))
 
 score = model.evaluate(x_test,
                        y_test,
